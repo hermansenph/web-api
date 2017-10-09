e35c8d4c69425c57de3e2999cf62640fd561eeef
hermansenph <hermansenph@gmail.com>
2017-10-09 12:54:55 -0700

    Created a POST request that uses insertOne #1

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const { MongoClient } = require('mongodb')
+const bodyParser = require('body-parser')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/data', (req, res) => {
+  console.log(req.body)
+  MongoClient.connect('mongodb://localhost/crud', (err, db) => {
+    if (err) {
+      console.error(err)
+      res.sendStatus(500)
+      process.exit(1)
+    }
+    const data = db.collection('data')
+    data.insertOne(req.body, (err, result) => {
+      if (err) {
+        console.error(err)
+        res.sendStatus(500)
+      }
+      else {
+        res.sendStatus(201)
+      }
+    })
+    db.close()
+  })
+})
+
+app.listen('3000', () => {
+  console.log('Listening on :3000...')
+})
