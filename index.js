b2160f39a5022d7609134c359a4ec486150278d8
hermansenph <hermansenph@gmail.com>
2017-10-09 13:08:16 -0700

    Created GET request that uses find method on database

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ const app = express()
 
 app.use(bodyParser.json())
 
+app.get('/data', (req, res) => {
+  MongoClient.connect('mongodb://localhost/crud', (err, db) => {
+    if (err) {
+      console.error(err)
+      res.sendStatus(500)
+      process.exit(1)
+    }
+    const data = db.collection('data')
+    data.find().toArray((err, result) => {
+      if (err) {
+        console.error(err)
+        res.sendStatus(500)
+      }
+      else {
+        res.send(result)
+      }
+    })
+  })
+})
+
 app.post('/data', (req, res) => {
   console.log(req.body)
   MongoClient.connect('mongodb://localhost/crud', (err, db) => {
