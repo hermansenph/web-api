2928890a26df5c6a2742d96d94dfa6dfbf608bdb
hermansenph <hermansenph@gmail.com>
2017-10-09 14:18:49 -0700

    Removed destructuring of ObjectId #3

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ObjectId } = require('mongodb')
+const { MongoClient } = require('mongodb')
 const bodyParser = require('body-parser')
 
 const app = express()
